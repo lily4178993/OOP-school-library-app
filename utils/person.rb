89e3aa687bef7ae26da81a8d322e08740f0c0b42
lily4178993 <nellytelli@gmail.com>
2023-11-17 16:23:19 +0000

    feat: Update Person class with corrected add_rental method

--- utils/person.rb
+++ utils/person.rb
@@ -2,16 +2,29 @@ require 'securerandom'
 require_relative '../interfaces/nameable'
 
 class Person < Nameable
-  attr_reader :id # Generates getters
+  attr_reader :id, :rentals # Generates getters
   attr_accessor :name, :age # Generates getters and setters
 
-  # Constructor initialization
+  # Initialize a new Person instance.
+  #
+  # Parameters:
+  # - age: The age of the person.
+  # - name: The name of the person (default is 'Unknown').
+  # - parent_permission: Permission from parents (default is true).
+  #
+  # Actions:
+  # - Call the parent class's constructor with additional parameters.
+  # - Set up the unique ID for the person.
+  # - Initialize an empty array for rentals.
+  #
+  # Returns: An instance of Person.
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
@@ -39,5 +52,20 @@ class Person < Nameable
   # Returns the name attribute of the Person, providing the correct name.
   def correct_name
     @name
+  end
+
+  # Add a rental record to the person.
+  #
+  # Parameters:
+  # - rental: The rental object to be added.
+  #
+  # Actions:
+  # - Add the rental to the person's rentals.
+  # - Set the person for the rental.
+  #
+  # Returns: None.
+  def add_rental(rental)
+    @rentals << rental
+    rental.person = self
   end
 end
