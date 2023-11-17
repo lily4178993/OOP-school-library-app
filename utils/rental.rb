99f71d8869229efecd34f2e9351b45354da1ca4a
lily4178993 <nellytelli@gmail.com>
2023-11-17 16:24:52 +0000

    feat: Implement Rental class with proper associations

---
+++ utils/rental.rb
@@ -0,0 +1,22 @@
+class Rental
+  attr_accessor :date
+  attr_reader :book, :person
+
+  # Initialize a new Rental instance.
+  #
+  # Parameters:
+  # - date: The date of the rental.
+  #
+  # Actions:
+  # - Set the rental date, book, and person.
+  # - Add the rental to the book's rentals unless it's already included.
+  # - Add the rental to the person's rentals unless it's already included.
+  #
+  # Returns: An instance of Rental.
+  def initialize(date)
+    @date = date
+    @book = book
+    @book.rentals << self unless @book.rentals.include?(self)
+    @person.rentals << self unless @person.rentals.include?(self)
+  end
+end
