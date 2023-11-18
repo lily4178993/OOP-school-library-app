4f4dd727ac93a350bc9edeb768d6e97bcd881da7
lily4178993 <nellytelli@gmail.com>
2023-11-18 14:35:46 +0000

    feat: Define main entry point in `main.rb`
    - Created the `main` method in `main.rb` to serve as the entry point for the console app.
    - Presented users with a list of options to perform.
    - Enabled users to choose an option and, if needed, provided a way to input parameters.
    - Implemented a graceful exit option for quitting the app.

--- main.rb
+++ main.rb
@@ -1,10 +1,47 @@
-load './utils/person.rb'
-load './utils/capitalize_decorator.rb'
-load './utils/trimmer_decorator.rb'
+require_relative 'src/app'
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+MENU_OPTIONS = {
+  1 => 'List all books',
+  2 => 'List all people',
+  3 => 'Create a book',
+  4 => 'Create a person',
+  5 => 'Create a rental',
+  6 => 'List all rentals for a given person id',
+  7 => 'Exit'
+}.freeze
+
+def app_menu
+  puts "\nWelcome to School Library App!\n\n"
+  MENU_OPTIONS.each { |key, value| puts "#{key} - #{value}" }
+  print 'Enter a number: '
+end
+
+def take_action(app, input)
+  case input
+  when 1 then app.list_of_books
+  when 2 then app.list_of_people
+  when 3 then app.create_book
+  when 4 then app.create_person
+  when 5 then app.create_rental
+  when 6 then app.list_of_rentals_per_person
+  end
+end
+
+def main
+  app = App.new
+  input = nil
+
+  until input == 7
+    app_menu
+    input = gets.chomp.to_i
+
+    unless MENU_OPTIONS.key?(input)
+      puts 'Invalid option. Please choose a number from the menu!'
+      next
+    end
+
+    take_action(app, input)
+  end
+end
+
+main
